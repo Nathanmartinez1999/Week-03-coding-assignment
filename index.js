697824f2e76821c026dddacd31e5afa9bdee138f
Nathanmartinez1999 <nmlobos1999@gmail.com>
2023-01-23 13:52:54 -0700

    next upload

--- index.js
+++ index.js
@@ -27,3 +27,99 @@ for (let i = 0; i < names.length; i++) {
 }
 console.log("sum of all name letters added togther", sumOfLetters);
 console.log("average number of letters in each name", sumOfLetters / names.length)
+
+console.log("question number 3:How do you access the last element orf an array");
+console.log("to access the last element of an array i did array  [array.lenth - 1]");
+
+console.log("Question number 4:how do you access the first element in a array");
+console.log("to access the first element in the array you can use [array[0]] the first element in the array is refered to by 0 since it is the first in the array");
+
+console.log("question number 05:");
+let nameLengths = [];
+
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i].length);
+    nameLengths.push(names[i].length);
+}
+console.log("nameLengths array rersult:", nameLengths);
+
+console.log("question number 06:");
+let sumOfLettersInName = 0
+for (let i = 0; i < nameLengths.length; i++) {
+    // console.log(nameLengths[i]);
+    sumOfLettersInName = sumOfLettersInName + nameLengths[i];
+    console.log(sumOfLettersInName);
+}
+
+console.log("question number 07:");
+
+function questionSeven(word, n) {
+    let solution = word.repeat(n);
+    console.log(solution);
+}
+questionSeven("hello", 3);
+
+console.log("question number 08:");
+let fullName = ['FirstName', 'lastName'];
+console.log(fullName);
+
+console.log("question number 09:")
+
+function isGreaterThan100(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i]);
+        sum = sum + array[i];
+        console.log("total", sum);
+    }
+    if (sum > 100) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+console.log("sum of all 3 numbers added up:");
+console.log(isGreaterThan100([100, 200, 300]));
+
+console.log("question number 10:");
+
+let numbers = new Array(2, 5, 10, 20, 15, 11);
+console.log(numbers);
+let length = numbers.length;
+
+let total = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    total = total + numbers[i];
+}
+console.log("Average of all numbers in the array:", total / length);
+
+console.log("question number 11:");
+
+let array1 = new Array(5, 10, 15, 20, 25);
+let lenth = array1.length;
+let totalNumbers = 0;
+for (let i = 0; i < array1.length; i++) {
+    totalNumbers = totalNumbers + array1[i];
+}
+console.log("average of array 1=", totalNumbers / length);
+
+let array2 = new Array(2, 4, 6, 8, 10);
+let lentgh = array2.length;
+let totalNumber = 0;
+for (let i = 0; i < array2.length; i++) {
+    totalNumber = totalNumber + array2[i];
+}
+console.log("total for array 2=", totalNumber / length);
+Boolean[array1 > array2] = true;
+console.log(array1 > array2);
+
+
+console.log("question number 12:");
+
+
+
+
+
+
